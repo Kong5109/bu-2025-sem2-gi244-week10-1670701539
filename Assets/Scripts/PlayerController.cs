a56f57f09fa20eafe1537e1513d9f8c57df03825
Kong5109 <kong8071.kk@gmail.com>
2026-03-20 15:56:56 +0700

    Exam04

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
 
     public bool gameOver = false;
     public bool isPlayerDashing = false;
-    //public int hp = 3;
+    public int hp = 3;
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -96,13 +96,22 @@ public class PlayerController : MonoBehaviour
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            Debug.Log("Game Over!");
-            gameOver = true;
-            playerAnim.SetBool("Death_b", true);
-            playerAnim.SetInteger("DeathType_int", 1);
-            explosionParticle.Play();
-            dirtParticle.Stop();
-            playerAudio.PlayOneShot(crashSfx);
+            hp--;
+            if (hp > 0)
+            {
+                hitParticle.Play();
+                Destroy(collision.gameObject);
+            }
+            else
+            {
+                Debug.Log("Game Over!");
+                gameOver = true;
+                playerAnim.SetBool("Death_b", true);
+                playerAnim.SetInteger("DeathType_int", 1);
+                explosionParticle.Play();
+                dirtParticle.Stop();
+                playerAudio.PlayOneShot(crashSfx);
+            }
         }
     }
 
